254b372fdf3b03671cd107903c2e96f8c1a32de7
Fran2333 <00161218@uca.edu.sv>
2020-05-20 12:43:15 -0600

    update view

--- app/src/main/java/com/pdm/atikapp/SpecialOrdersFragment.kt
+++ app/src/main/java/com/pdm/atikapp/SpecialOrdersFragment.kt
@@ -1,15 +1,22 @@
 package com.pdm.atikapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.checkSelfPermission
+import  kotlinx.android.synthetic.main.fragment_special_orders.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class SpecialOrdersFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,10 @@ class SpecialOrdersFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_special_orders, container, false)
     }
+
+
+
+
 
 
 
