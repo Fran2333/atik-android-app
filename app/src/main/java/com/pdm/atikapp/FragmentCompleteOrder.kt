c9536426ca60f8d7a73b23f1704e1fb7698eadaa
Fran2333 <00161218@uca.edu.sv>
2020-05-21 17:03:41 -0600

    update

--- app/src/main/java/com/pdm/atikapp/FragmentCompleteOrder.kt
+++ app/src/main/java/com/pdm/atikapp/FragmentCompleteOrder.kt
@@ -1,15 +1,22 @@
 package com.pdm.atikapp
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_complete_order.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class FragmentCompleteOrder : Fragment() {
+
+    var formate = SimpleDateFormat("dd MM, YYYY",Locale.US)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,21 @@ class FragmentCompleteOrder : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_complete_order, container, false)
+
+        btn_day.setOnClickListener{
+            val now = Calendar.getInstance()
+            val datePicker = DatePickerDialog(context!!, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                val selectDate = Calendar.getInstance()
+                selectDate.set(Calendar.YEAR,year)
+                selectDate.set(Calendar.MONTH,month)
+                selectDate.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                val date = formate.format(selectDate.time)
+                Toast.makeText(context,"dia:"+ date,Toast.LENGTH_SHORT).show()
+            },
+                now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DAY_OF_MONTH))
+            datePicker.show()
+        }
+
     }
 
 }
