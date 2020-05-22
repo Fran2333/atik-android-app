6c4cf57a4ba04e1c6750474ca6a8e09656bb4a5c
AlexisMejia <00090718@uca.edu.sv>
2020-05-21 19:20:29 -0600

    Promotionview: body fixed

---
+++ app/src/main/java/com/pdm/atikapp/PromotionAdapter.kt
@@ -0,0 +1,46 @@
+package com.pdm.atikapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import java.security.AccessControlContext
+
+class PromotionAdapter(
+    private val context: Context,
+    private val image: Array<Int>,
+    private val category: ArrayList<String>
+): BaseAdapter() {
+
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = inflater.inflate(R.layout.activity_grid_element,null)
+
+        val img: ImageView = view.findViewById(R.id.promotion_img)
+        img.setImageResource(image[position])
+
+        val name: TextView = view.findViewById(R.id.promotion_name)
+        name.setText(category[position])
+
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return category[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return category.size
+    }
+
+
+}
--- app/src/main/java/com/pdm/atikapp/PromotionFragment.kt
+++ app/src/main/java/com/pdm/atikapp/PromotionFragment.kt
@@ -5,18 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
+import java.util.ArrayList
 
 /**
  * A simple [Fragment] subclass.
  */
 class PromotionFragment : Fragment() {
 
+    var namePro = arrayListOf<String>(
+        "Promotion 1",
+        "Promotion 2",
+        "Promotion 3",
+        "Promotion 4",
+        "Promotion 5",
+        "Promotion 6"
+    )
+
+    var imageArray = arrayOf<Int>(
+        R.drawable.icon,
+        R.drawable.icontwo,
+        R.drawable.iconthree,
+        R.drawable.iconf,
+        R.drawable.iconff,
+        R.drawable.iconst
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promotion, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_promotion, container, false)
+
+        var gv = view.findViewById<GridView>(R.id.ppGrid)
+        val adapter= PromotionAdapter(context!!, this.imageArray, this.namePro)
+        gv.adapter = adapter
+
+        gv.numColumns = 2
+        gv.horizontalSpacing = 15
+        gv.verticalSpacing = 25
+        gv.stretchMode = GridView.STRETCH_COLUMN_WIDTH
+        return view
     }
 
 }
