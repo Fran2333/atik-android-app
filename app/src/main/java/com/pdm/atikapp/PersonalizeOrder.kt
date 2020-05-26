7e651e0b15cdd2efd84f009c75d4a8cd1fc853e8
crisarevalom <00209718@uca.edu.sv>
2020-05-26 01:06:31 -0600

    views fixed

--- app/src/main/java/com/pdm/atikapp/PersonalizeOrder.kt
+++ app/src/main/java/com/pdm/atikapp/PersonalizeOrder.kt
@@ -9,14 +9,14 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class Personalize : Fragment() {
+class PersonalizeOrder : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personalize, container, false)
+        return inflater.inflate(R.layout.fragment_personalize_order, container, false)
     }
 
 }
